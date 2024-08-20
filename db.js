617805d77860bf6b1bf54b281797f6eba4ba7f77
Naphees-7667 <naphees6200@gmail.com>
2024-08-20 23:20:37 +0530

    Hotel url

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const mongodbURL = process.env.mongodbURL;
-
+// const mongodbURL = process.env.mongodbURL;
+const mongodbURL = 'mongodb://127.0.0.1:27017/ResturentDB';
 mongoose.connect(mongodbURL);
 
 const db = mongoose.connection; 
